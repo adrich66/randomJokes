e5ea33804a328b91e970b92b43c6482263172ef1
Adrià Moya Cangueiro <adri@MacBook-Air-de-Adria.local>
2022-07-14 10:46:53 +0200

    Exercice 1

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const API_URL = 'https://icanhazdadjoke.com/';
 const HTMLelement = document.getElementById("joke");
 
 async function getJokes(){
-    fetch('${API_URL}')
+    const joke = await fetch(API_URL,{
+        headers:{
+            Accept:"application/json",
+        },
+    }).then((response)=> response.json())
+
+    console.log(joke);
+
 } 
-.then((response)=> response.json())
 
