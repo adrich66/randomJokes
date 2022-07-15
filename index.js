1cf8ec7978a239f3e1bccf2e5b4f9ce6a21d27c8
Adrià Moya Cangueiro <adri@MacBook-Air-de-Adria.local>
2022-07-15 20:06:54 +0200

    Exercice 3 and Level 1 done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const API_URL = 'https://icanhazdadjoke.com/';
-const HTMLelement = document.getElementById("joke");
+const spaceForJoke = document.getElementById("joke");
+const reportJokes = [];
+
 
 async function getJokes(){
     const jokes = await fetch(API_URL,{
@@ -10,9 +12,31 @@ async function getJokes(){
 
     console.log(jokes);
 
-    HTMLelement.innerHTML = jokes.joke;
+    spaceForJoke.innerHTML = jokes.joke;
+    
+    document.getElementById("buttons").innerHTML = '<button class="btn2" onclick="getJokes(); trackingJokes(1);"><h1>😓</h1></button><button class="btn2" onclick="getJokes(); trackingJokes(2);"><h1>😐</h1></button><button class="btn2" onclick="getJokes(); trackingJokes(3);"><h1>😆</h1></button>';
 
 } 
+
+document.getElementById("start").addEventListener("click",function(){
+	this.value="Next Joke...";
+});
+
+function trackingJokes(number){
+
+    const jokeIn = document.getElementById("joke").innerHTML;
+    const score = parseInt(number);
+    const currentDate = new Date();
+    let currentDay = currentDate.toISOString();
+   
+    reportJokes.push({
+        joke: jokeIn,
+        score: score,
+        date: currentDay
+    });
+
+    console.log(reportJokes);
+}
 
 
 
